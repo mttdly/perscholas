a653d4c50f88fef97012840aa3c269f3f7634dae
Matthew <matthewaarondaly@gmail.com>
2022-12-05 16:19:40 -0500

    update

---
+++ perscholas/src/java/pa/scanner/ScannerPartTwo.java
@@ -0,0 +1,22 @@
+package org.perscholas.jdtek.java.pa;
+
+// https://www.hackerrank.com/challenges/java-stdin-stdout/problem
+// Java Stdin and Stdout I
+
+import java.util.Scanner;
+
+public class ScannerPartTwo {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
